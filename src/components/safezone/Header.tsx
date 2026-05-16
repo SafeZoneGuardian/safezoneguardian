99a2f965a07ab2c600e5c24cd5c16a6219d55a30
SafeZoneGuardian <rolf08650@gmail.com>
2026-05-16 16:14:33 +0200

    Aktualisieren von Header.tsx

--- src/components/safezone/Header.tsx
+++ src/components/safezone/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { MapPin, Plus } from 'lucide-react';
 
 const SZGLogo = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400" width="100%" height="100%">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 440" width="100%" height="100%">
     <defs>
       <linearGradient id="hShieldOuter" x1="0%" y1="0%" x2="100%" y2="100%">
         <stop offset="0%" stopColor="#4a6fa5" />
@@ -38,15 +38,6 @@ const SZGLogo = () => (
       <filter id="hShadow">
         <feDropShadow dx="0" dy="8" stdDeviation="12" floodColor="#000d1f" floodOpacity="0.6"/>
       </filter>
-      <filter id="hLogoGlow" x="-40%" y="-40%" width="180%" height="180%">
-        <feGaussianBlur stdDeviation="6" result="blur" />
-        <feFlood floodColor="#00e5ff" floodOpacity="0.6" result="color" />
-        <feComposite in="color" in2="blur" operator="in" result="glow" />
-        <feMerge>
-          <feMergeNode in="glow" />
-          <feMergeNode in="SourceGraphic" />
-        </feMerge>
-      </filter>
       <clipPath id="hShieldClip">
         <path d="M200 45 L310 85 L310 195 Q310 295 200 355 Q90 295 90 195 L90 85 Z"/>
       </clipPath>
@@ -81,7 +72,7 @@ const Header: React.FC<HeaderProps> = ({ onReport, onScrollTo }) => {
           <div className="relative">
             <div className="absolute inset-0 bg-cyan-400/30 blur-2xl rounded-full group-hover:bg-cyan-400/60 transition-all duration-300 scale-110" />
             <div className="absolute inset-0 bg-blue-500/20 blur-xl rounded-full animate-pulse" />
-            <div className="relative w-16 h-16 drop-shadow-[0_0_12px_rgba(0,229,255,0.8)]">
+            <div className="relative w-20 h-16 drop-shadow-[0_0_12px_rgba(0,229,255,0.8)]">
               <SZGLogo />
             </div>
           </div>
